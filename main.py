ddc31b4a7c307e7df84ee33a758bf6b54989e368
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-03-31 12:51:07 +0900

    update

--- main.py
+++ main.py
@@ -42,11 +42,10 @@ if __name__ == "__main__":
             ),
             params=Param(
                 weight_decay=2e-4,
-                learning_rate_fn=lambda global_step: tf.train.exponential_decay(
-                    learning_rate=0.1,
+                learning_rate_fn=lambda global_step: tf.train.piecewise_constant(
                     global_step=global_step,
-                    decay_steps=2500000,
-                    decay_rate=0.1
+                    boundaries=[50000 * num_epochs // args.batch_size for num_epochs in [100, 150, 200]],
+                    values=[0.1 * decay_rate for decay_rate in [1.0, 0.1, 0.01, 0.001]]
                 ),
                 momentum=0.9
             )
