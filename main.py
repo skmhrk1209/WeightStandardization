94b7c17eae8aed1f62dcd028d95aea23e2261f03
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-04-05 00:54:19 +0900

    update

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import operator
 import glob
 import hooks
 from param import Param
+from model import Classifier
 from dataset import cifar10_input_fn
 from network import ResNetWithWeightStandardization
 
@@ -25,20 +26,32 @@ tf.logging.set_verbosity(tf.logging.INFO)
 if __name__ == "__main__":
 
     estimator = tf.estimator.Estimator(
-        model_fn=lambda features, labels, mode, params: ResNetWithWeightStandardization(
-            conv_param=Param(filters=32, kernel_size=[3, 3], strides=[1, 1]),
-            pool_param=None,
-            residual_params=[
-                Param(filters=32, strides=[1, 1], blocks=3),
-                Param(filters=64, strides=[2, 2], blocks=3),
-                Param(filters=128, strides=[2, 2], blocks=3),
-            ],
-            num_classes=10
+        model_fn=lambda features, labels, mode, params: Classifier(
+            network=ResNetWithWeightStandardization(
+                conv_param=Param(filters=32, kernel_size=[3, 3], strides=[1, 1]),
+                pool_param=None,
+                residual_params=[
+                    Param(filters=32, strides=[1, 1], blocks=3),
+                    Param(filters=64, strides=[2, 2], blocks=3),
+                    Param(filters=128, strides=[2, 2], blocks=3),
+                ],
+                num_classes=10
+            )
         )(features, labels, mode, Param(params)),
         model_dir=args.model_dir,
         config=tf.estimator.RunConfig(
             save_summary_steps=100,
             save_checkpoints_steps=1000
+        ),
+        params=dict(
+            weight_decay=2e-4,
+            learning_rate=lambda global_step: tf.train.piecewise_constant(
+                x=global_step,
+                boundaries=[50000 * num_epochs // args.batch_size for num_epochs in [100, 150, 200]],
+                values=[0.1 * decay_rate for decay_rate in [1.0, 0.1, 0.01, 0.001]]
+            ),
+            momentum=0.9,
+            use_nesterov=True
         )
     )
 
