c23f4b4d44af7481ecf0f825588540b1ba11f9fc
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-04-04 21:14:01 +0900

    update

--- main.py
+++ main.py
@@ -45,7 +45,11 @@ if __name__ == "__main__":
     if args.train:
 
         logits = classifier(images)
-        losses = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits, reduction=None)
+        losses = tf.losses.sparse_softmax_cross_entropy(
+            labels=labels,
+            logits=logits,
+            reduction=reduction=tf.losses.Reduction.NONE
+        )
         losses += tf.add_n([tf.nn.l2_loss(variable) for variable in tf.trainable_variables()]) * 2e-4
 
         global_step = tf.train.create_global_step()
