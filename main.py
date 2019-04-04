d6927b53df13b9e427c0392d97ea4b3fb030d150
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-04-05 00:53:23 +0900

    update

--- main.py
+++ main.py
@@ -24,98 +24,32 @@ tf.logging.set_verbosity(tf.logging.INFO)
 
 if __name__ == "__main__":
 
-    classifier = ResNetWithWeightStandardization(
-        conv_param=Param(filters=32, kernel_size=[3, 3], strides=[1, 1]),
-        pool_param=None,
-        residual_params=[
-            Param(filters=32, strides=[1, 1], blocks=3),
-            Param(filters=64, strides=[2, 2], blocks=3),
-            Param(filters=128, strides=[2, 2], blocks=3),
-        ],
-        num_classes=10,
-    )
-
-    images, labels = cifar10_input_fn(
-        filenames=glob.glob(args.filenames),
-        batch_size=args.batch_size,
-        num_epochs=args.num_epochs if args.train else 1,
-        shuffle=True if args.train else False,
+    estimator = tf.estimator.Estimator(
+        model_fn=lambda features, labels, mode, params: ResNetWithWeightStandardization(
+            conv_param=Param(filters=32, kernel_size=[3, 3], strides=[1, 1]),
+            pool_param=None,
+            residual_params=[
+                Param(filters=32, strides=[1, 1], blocks=3),
+                Param(filters=64, strides=[2, 2], blocks=3),
+                Param(filters=128, strides=[2, 2], blocks=3),
+            ],
+            num_classes=10
+        )(features, labels, mode, Param(params)),
+        model_dir=args.model_dir,
+        config=tf.estimator.RunConfig(
+            save_summary_steps=100,
+            save_checkpoints_steps=1000
+        )
     )
 
     if args.train:
 
-        logits = classifier(images)
-
-        losses = tf.losses.sparse_softmax_cross_entropy(
-            labels=labels,
-            logits=logits,
-            reduction=tf.losses.Reduction.NONE
+        estimator.train(
+            input_fn=functools.partial(
+                cifar10_input_fn,
+                filenames=glob.glob(args.filenames),
+                batch_size=args.batch_size,
+                num_epochs=args.num_epochs,
+                shuffle=True
+            )
         )
-        losses += tf.add_n([
-            tf.nn.l2_loss(variable)
-            for variable in tf.trainable_variables()
-        ]) * 2e-4
-
-        global_step = tf.train.create_global_step()
-        optimizer = tf.train.MomentumOptimizer(
-            learning_rate=tf.train.piecewise_constant(
-                x=global_step,
-                boundaries=[50000 * num_epochs // args.batch_size for num_epochs in [100, 150, 200]],
-                values=[0.1 * decay_rate for decay_rate in [1.0, 0.1, 0.01, 0.001]]
-            ),
-            momentum=0.9,
-            use_nesterov=True
-        )
-
-        gradients = [
-            tf.reduce_mean(gradients, axis=0)
-            for gradients in zip(*[
-                list(zip(*optimizer.compute_gradients(loss)))[0]
-                for loss in tf.unstack(losses, axis=0)
-            ])
-        ]
-
-        train_op = optimizer.apply_gradients(
-            grads_and_vars=zip(gradients, tf.trainable_variables()),
-            global_step=global_step
-        )
-
-        loss = tf.reduce_mean(losses)
-
-        with tf.train.SingularMonitoredSession(
-            scaffold=tf.train.Scaffold(
-                init_op=tf.global_variables_initializer(),
-                local_init_op=tf.group(
-                    tf.local_variables_initializer(),
-                    tf.tables_initializer()
-                )
-            ),
-            checkpoint_dir=args.model_dir,
-            hooks=[
-                tf.train.CheckpointSaverHook(
-                    checkpoint_dir=args.model_dir,
-                    save_steps=1000,
-                    saver=tf.train.Saver(
-                        max_to_keep=10,
-                        keep_checkpoint_every_n_hours=12,
-                    )
-                ),
-                tf.train.SummarySaverHook(
-                    output_dir=args.model_dir,
-                    save_steps=100,
-                    summary_op=tf.summary.merge([
-                        tf.summary.scalar("loss", loss)
-                    ])
-                ),
-                tf.train.LoggingTensorHook(
-                    tensors={
-                        "global_step": global_step,
-                        "loss": loss
-                    },
-                    every_n_iter=100
-                )
-            ]
-        ) as session:
-
-            while not session.should_stop():
-                session.run(train_op)
---
+++ model.py
@@ -0,0 +1,60 @@
+import tensorflow as tf
+
+
+class Classifier(object):
+
+    def __init__(self, network):
+
+        self.network = network
+
+    def __call__(self, images, labels, mode, params):
+
+        logits = self.network(images, training=mode == tf.estimator.ModeKeys.TRAIN)
+
+        losses = tf.losses.sparse_softmax_cross_entropy(
+            labels=labels,
+            logits=logits,
+            reduction=tf.losses.Reduction.NONE
+        )
+        losses += tf.add_n([
+            tf.nn.l2_loss(variable)
+            for variable in tf.trainable_variables()
+        ]) * params.weight_decay
+
+        loss = tf.reduce_mean(losses)
+
+        if mode == tf.estimator.ModeKeys.TRAIN:
+
+            optimizer = tf.train.MomentumOptimizer(
+                learning_rate=(
+                    params.learning_rate(tf.train.get_or_create_global_step())
+                    if callable(params.learning_rate) else params.learning_rate
+                ),
+                momentum=params.momentum,
+                use_nesterov=params.use_nesterov
+            )
+
+            with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
+
+                def generator(losses, iterations):
+                    global variables
+                    for losses in tf.split(losses, iterations, axis=0):
+                        loss = tf.reduce_mean(losses)
+                        gradients, variables = zip(*optimizer.compute_gradients(loss))
+                        yield gradients
+
+                gradients = [
+                    tf.reduce_mean(gradients, axis=0)
+                    for gradients in zip(*generator())
+                ]
+
+                train_op = optimizer.apply_gradients(
+                    grads_and_vars=zip(gradients, variables),
+                    global_step=tf.train.create_global_step()
+                )
+
+            return tf.estimator.EstimatorSpec(
+                mode=mode,
+                loss=loss,
+                train_op=train_op
+            )
